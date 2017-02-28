02db62923029bcd3a13b1874179dea0afaf87d66
sriwahyuni-aj <ajsriwahyuni@gmail.com>
2017-02-28 23:33:37 +0700

    Add files via upload

---
+++ mahasiswa.java
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Latihan Java Script </title>
+<h4><i><u><font face="times new roman" size="+2" color ="#993300">Biodata Mahasiswa</font></u></i></h4>
+<body><pre><script language ="javascript">
+document.write("Nama Lengkap                : Sri Wahyuni Aj </br>");
+document.write("NIM                         : 10114393 </br>" );
+document.write("Alamat                      : Jl. sekeloa selatan No.18 </br>");
+document.write("<br>======================================Sri Wahyuni Aj========================================="+"</b>");
+
+</script>
+</body>
+</head>
+</html>
